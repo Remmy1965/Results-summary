ed7a3d0b13a48c08697dc354111c4aff4d7b1062
Remmy <akeemquadri00@gmail.com>
2023-03-28 00:19:24 -0700

    Extra feels

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ module.exports = {
   theme: {
     screens: {
       sm: '375px',
-      lg: '1440px'
+      lg: '1440px',
     },
     extend: {
       fontFamily: {
-        hanken: ['Hanken Grotesk', 'sans-serif']
+        hanken: ['Hanken Grotesk', 'sans-serif'],
       },
       colors: {
         //PRIMARY COLORS
@@ -22,13 +22,18 @@ module.exports = {
         lightRoyalBlue: 'hsl(241, 81%, 54%)', //Background
         violetBlue: 'hsla(256, 72%, 46%, 1)', //Circle
         persianBlue: 'hsla(241, 72%, 46%, 0)', //circle
+        lightRedG: 'hsla(0, 97%, 85%, 0.989)',
+        orangeYellowG: 'hsla(39, 92%, 85%, 0.973)',
+        greenTealG: 'hsla(165, 17%, 86%, 0.998)',
+        cobaltBlueG: 'hsla(240, 12%, 86%, 0.975)',
+        violetBlueG: 'hsla(256, 45%, 49%, 0.981)',
 
         //NEUTRAL
         paleBlue: 'hsl(221, 100%, 96%)',
         lightLavender: 'hsl(241, 100%, 89%)',
         darkGrayBlue: 'hsl(224, 30%, 27%)',
-      }
+      },
     },
   },
   plugins: [],
-}
+};
